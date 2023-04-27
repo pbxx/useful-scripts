6fecb2333c22959038c524e11da55767437fbc41
pbxx <airlew@gmail.com>
2023-04-27 12:43:58 -0600

    update scripts

---
+++ join-domain/join-all-user-access.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# This script joins a Debian system to an Active Directory Domain in order to enable authentication
+# It also installs all required dependencies to do so
+
+if [[ $EUID > 0 ]]; then  
+	echo "ERROR: This script must be run with sudo..."
+	exit 1
+else
+	echo "This script joins a Debian system to an Active Directory Domain in order to enable authentication"; echo $newline
+	# Read domain
+	echo -n "Please input full domain name (ex. domain.contoso.net):"; read domain; echo $newline
+	# Read username
+	echo -n "Username to use in order to join the domain:"; read username; echo $newline
+	
+	#install all required deps for a typical AD join
+	dpkg --verify realmd || apt-get install realmd -y
+	dpkg --verify sssd || apt-get install sssd -y
+	dpkg --verify sssd-tools || apt-get install sssd-tools -y
+	dpkg --verify libnss-sss || apt-get install libnss-sss -y
+	dpkg --verify libpam-sss || apt-get install libpam-sss -y
+	dpkg --verify adcli || apt-get install adcli -y
+	dpkg --verify samba-common-bin || apt-get install samba-common-bin -y
+	dpkg --verify sssd || aptitude install sssd -y
+	
+	
+	echo "All required dependencies are installed!"
+	echo "Joining domain now..."; echo $newline
+	
+	#join domain
+	realm join $domain -U "'$username'" --install='/' --verbose
+
+	#Allow all AD users to log in
+	realm permit --realm $domain --all
+fi
